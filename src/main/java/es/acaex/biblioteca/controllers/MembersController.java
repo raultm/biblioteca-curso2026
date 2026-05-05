de97772dca466616698f93eb23b1458ddf8cfdb1
Raul Tierno <raultm@gmail.com>
2026-05-05 19:08:00 +0200

    feat: :sparkles: listado, creacion y detalle de socio

---
+++ src/main/java/es/acaex/biblioteca/controllers/MembersController.java
@@ -0,0 +1,36 @@
+package es.acaex.biblioteca.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import es.acaex.biblioteca.models.Member;
+import es.acaex.biblioteca.repositories.MembersRepository;
+
+@RestController
+@RequestMapping("members")
+public class MembersController {
+    
+    @Autowired MembersRepository repository;
+
+    @GetMapping
+    public List<Member> findAll() {
+        return repository.findAll();
+    }
+    
+    @PostMapping
+    public Member save(Member member) { 
+        return repository.save(member); 
+    }
+
+    @GetMapping("{memberId}")
+    public Member findByid(@PathVariable("memberId")Long memberId) {
+        return repository.findById(memberId).orElseThrow();
+    }
+
+}
---
+++ src/main/java/es/acaex/biblioteca/models/Member.java
@@ -0,0 +1,25 @@
+package es.acaex.biblioteca.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "members")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+ 
+    private String username;
+    private String email;
+}
