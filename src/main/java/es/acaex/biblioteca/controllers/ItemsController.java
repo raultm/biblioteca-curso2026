2290023b6c41a446488b42f75599e9c3d9261a79
Raul Tierno <raultm@gmail.com>
2026-05-07 10:00:53 +0200

    feat(copies): añadir copia a elemento y listado de copias de un elemento

--- src/main/java/es/acaex/biblioteca/controllers/ItemsController.java
+++ src/main/java/es/acaex/biblioteca/controllers/ItemsController.java
@@ -1,5 +1,6 @@
 package es.acaex.biblioteca.controllers;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import es.acaex.biblioteca.models.Copy;
 import es.acaex.biblioteca.models.Item;
+import es.acaex.biblioteca.repositories.CopiesRepository;
 import es.acaex.biblioteca.repositories.ItemsRepository;
 
 @RestController
@@ -18,20 +21,36 @@ import es.acaex.biblioteca.repositories.ItemsRepository;
 public class ItemsController {
 
     @Autowired
-    ItemsRepository repository;
+    ItemsRepository itemsRepository;
+    @Autowired
+    CopiesRepository copiesRepository;
 
     @PostMapping
     public Item save(@RequestBody Item item) {
-        return repository.save(item);
+        return itemsRepository.save(item);
     }
 
     @GetMapping
     public List<Item> get() {
-        return repository.findAll();
+        return itemsRepository.findAll();
     }
 
     @GetMapping("{itemId}")
     public Item findBy(@PathVariable("itemId") Long itemId) {
-        return repository.findById(itemId).orElseThrow();
+        return itemsRepository.findById(itemId).orElseThrow();
+    }
+
+    @PostMapping("{itemId}/copies")
+    public Copy crearCopia(@PathVariable("itemId") Long itemId) {
+        return copiesRepository.save(Copy.builder()
+                .item(itemsRepository.findById(itemId).orElseThrow())
+                .acquiredAt(new Date())
+                .reservedBy("")
+                .build());
+    }
+
+    @GetMapping("{itemId}/copies")
+    public List<Copy> listarCopias(@PathVariable("itemId") Long itemId) {
+        return itemsRepository.findById(itemId).orElseThrow().getCopies();
     }
 }
